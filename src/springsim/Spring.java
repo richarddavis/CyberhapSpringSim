2cfc06f423954e5d4f312bcd4fc812b8a53bd9f6
Gordon Minaker <gordonminaker@hotmail.com>
2015-08-14 14:50:15 -0700

    Adding Spring and SpringCollection class templates

---
+++ src/springsim/Spring.java
@@ -0,0 +1,15 @@
+package springsim;
+
+public class Spring {
+
+	int x;
+	int y;
+	int currentLen;
+	int originalLen;
+	int k;
+	
+	public Spring(){
+		// constructor
+	}
+	
+}
---
+++ src/springsim/SpringCollection.java
@@ -0,0 +1,14 @@
+package springsim;
+
+import java.util.ArrayList;
+
+public class SpringCollection {
+	
+	ArrayList<Spring> springs;
+	Spring activeSpring; 
+	
+	public SpringCollection(){
+		springs = new ArrayList<Spring>();
+	}
+	
+}
