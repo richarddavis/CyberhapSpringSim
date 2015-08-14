435017119eb7c6c513066db6e251ad6f3205ec3e
Gordon Minaker <gordonminaker@hotmail.com>
2015-08-14 15:45:32 -0700

    changes to constructor in Main class; resolves errors

--- src/springsim/Main.java
+++ src/springsim/Main.java
@@ -6,15 +6,15 @@ import shiffman.box2d.Box2DProcessing;
 public class Main extends PApplet {
 
 	Box2DProcessing box2d;
-	Spring s1 = new Spring(100, 100, 40, 200);
-	SpringCollection sc = new SpringCollection();
-	Hand hand = new Hand(this, 300, 300);
+	Spring s1;
+	SpringCollection sc;
+	Hand hand;
 	
 	public void setup() {
 		box2d = new Box2DProcessing(this);
 		box2d.createWorld();
 		
-		hand = new Hand();
+		hand = new Hand(this, 300, 300);
 
 		s1 = new Spring(100, 100, 40, 200, this, box2d);
 		s1.bind(hand.getX(), hand.getY(), hand);
