c014b4984e918500db76c84b31d28be071b925b1
Gordon Minaker <gordonminaker@hotmail.com>
2015-08-14 15:40:22 -0700

    implementing Spring class and corresponding changes to Main class

--- src/springsim/Main.java
+++ src/springsim/Main.java
@@ -1,13 +1,26 @@
 package springsim;
 
 import processing.core.PApplet;
+import shiffman.box2d.Box2DProcessing;
 
 public class Main extends PApplet {
-
-	Spring s1 = new Spring(100, 100, 40, 200);
-	SpringCollection sc = new SpringCollection();
+	
+	Box2DProcessing box2d;
+	Spring s1;
+	SpringCollection sc;
+	Hand hand;
 	
 	public void setup() {
+		box2d = new Box2DProcessing(this);
+		box2d.createWorld();
+		
+		hand = new Hand();
+
+		s1 = new Spring(100, 100, 40, 200, this, box2d);
+		s1.bind(hand.getX(), hand.getY(), hand);
+		
+		sc = new SpringCollection();
+		
 		size(1000,800);
 		background(255);
 	}
